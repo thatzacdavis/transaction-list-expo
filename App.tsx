7db0cd4dcc97170fb655b1d526be099449c42ddb
Zac Davis <thatzacdavis@gmail.com>
2023-01-21 17:52:09 -0500

    Add Header

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Header } from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header amountFound={0} />
     </View>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import { Text } from '@rneui/themed';
+
+export const Header = ({ amountFound }: { amountFound : number}) => {
+    return (
+        <Text h3>Found {amountFound} transactions</Text>
+    )
+}
